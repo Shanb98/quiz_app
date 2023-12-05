42384eb82c3e701bf1f856b79ffcb16eff461e36
Shanb98 <shanbasnayake@hotmail.com>
2023-12-05 23:24:03 +0530

    QuestionScreen2

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -10,39 +10,6 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              questions[currentQuestionIndex].question,
-              textAlign: TextAlign.center,
-              style: const TextStyle(color: Colors.white, fontSize: 28),
-            ),
-            ...questions[currentQuestionIndex]
-                .getRandomAnswers()
-                .map((answer) => AnswerButton(
-                    value: answer,
-                    onAnswer: () {
-                      answerQuestion(answer);
-                    })),
-            const SizedBox(
-              height: 40,
-            ),
-            TextButton(
-                onPressed: () {
-                  widget.onAction('start');
-                },
-                child: const Text(
-                  'Back to Start',
-                  style: TextStyle(fontSize: 18, color: Colors.amber),
-                ))
-          ],
-        ),
-      ),
-    );  }
+    return const Placeholder();
+  }
 }
